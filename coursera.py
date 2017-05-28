4a429c9e4a5a39f384679595ff02bb448db99f69
Ranc58 <rancvova@gmail.com>
2017-05-28 16:51:16 +0300

    add get_courses_list for create courses URLs list

--- coursera.py
+++ coursera.py
@@ -1,7 +1,15 @@
+import re
+from urllib import request
+from lxml import etree
 
 
-def get_courses_list():
-    pass
+def get_courses_list(url):
+    context = etree.iterparse(url)
+    url_list_of_courses = []
+    for action, course_url in context:
+        if re.findall(r'\w', course_url.text):
+            url_list_of_courses.append(course_url.text)
+    print(url_list_of_courses)
 
 
 def get_course_info(course_slug):
@@ -13,4 +21,5 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    url=request.urlopen('http://www.coursera.org/sitemap~www~courses.xml')
+    get_courses_list(url)
