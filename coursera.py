293785aa28948bd142605f5480841b46bd0e70a9
Ranc58 <rancvova@gmail.com>
2017-05-29 20:55:12 +0300

    edit get_courses_info_list for minimize code and return list of courses info dicts

--- coursera.py
+++ coursera.py
@@ -13,37 +13,26 @@ def get_random_courses_list(url):
     return random_courses_list
 
 
-def get_course_info(random_courses_list):
-    #course_list=[]
+def get_courses_info_list(random_courses_list):
+    courses_info_list = []
     for course in random_courses_list:
-        doc=requests.get(course)
-        course_name=html.fromstring(doc.content)
-        course_info=''.join(course_name.xpath('//p[@class="body-1-text course-description"]/text()'))
-
-"""        
-        course_info=[]
         doc = requests.get(course)
-        soup = bs(''.join(doc.text), "html.parser")
-        course_name = soup.find('h1', {'class': 'title display-3-text'})
-        print(course_name.text)
-        #course_info.append({'course_name':course_name.text})
-        about = soup.find('p', {'class': 'body-1-text course-description'})
-        print(about.text)
-        #course_info.append({'about_course': about})
-        starting_date = soup.find('div', {'class': 'startdate rc-StartDateString caption-text'})
-        print(starting_date.text)
-        #course_info.append({'strarting_date':starting_date.text})
-        language = soup.find('div', {'class': 'rc-Language'})
-        print(language.text)
-        #course_info.append({'language':language.text})
-        rate = soup.find('div', {'class': 'ratings-text bt3-visible-xs'})
-        if rate:
-            #course_info.append({'rate':rate.text})
-            print(rate.text)
-        #course_info.append({'course_duration':len(soup.findAll('div', {'class': 'week'}))})
-        #course_list.append(course_info)
-    #print(len(course_list))
-"""
+        course_content = html.fromstring(doc.content)
+        course_name = '//h1[@class="title display-3-text"]/text()'
+        start_date = '//div[@class="startdate rc-StartDateString caption-text"]/span/text()'
+        course_lang = '//div[@class="rc-Language"]/text()'
+        course_rate = '//div[@class="ratings-text bt3-visible-xs"]/text()'
+        course_duration = 'count(//*[@class="week"])'
+        course_info = {
+            'course_name': ''.join(course_content.xpath(course_name)),
+            'starting_date': ''.join(course_content.xpath(start_date)),
+            'course_lang': ''.join(course_content.xpath(course_lang)),
+            'course_rate': ''.join(course_content.xpath(course_rate)),
+            'course_duration': course_content.xpath(course_duration)
+        }
+        courses_info_list.append(course_info)
+    return courses_info_list
+
 
 def output_courses_info_to_xlsx(filepath):
     pass
@@ -52,4 +41,4 @@ def output_courses_info_to_xlsx(filepath):
 if __name__ == '__main__':
     url = 'http://www.coursera.org/sitemap~www~courses.xml'
     random_courses_list = get_random_courses_list(url)
-    get_course_info(random_courses_list)
+    get_courses_info_list(random_courses_list)
