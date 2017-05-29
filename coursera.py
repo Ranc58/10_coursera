f29749216fc24f75ce0026551991cb229f1666fc
Ranc58 <rancvova@gmail.com>
2017-05-29 16:50:54 +0300

    start rebuild get_course_info for minimize code

--- coursera.py
+++ coursera.py
@@ -14,22 +14,36 @@ def get_random_courses_list(url):
 
 
 def get_course_info(random_courses_list):
+    #course_list=[]
     for course in random_courses_list:
+        doc=requests.get(course)
+        course_name=html.fromstring(doc.content)
+        course_info=''.join(course_name.xpath('//p[@class="body-1-text course-description"]/text()'))
+
+"""        
+        course_info=[]
         doc = requests.get(course)
         soup = bs(''.join(doc.text), "html.parser")
         course_name = soup.find('h1', {'class': 'title display-3-text'})
-        print('\n', course_name.text)
+        print(course_name.text)
+        #course_info.append({'course_name':course_name.text})
         about = soup.find('p', {'class': 'body-1-text course-description'})
         print(about.text)
+        #course_info.append({'about_course': about})
         starting_date = soup.find('div', {'class': 'startdate rc-StartDateString caption-text'})
         print(starting_date.text)
+        #course_info.append({'strarting_date':starting_date.text})
         language = soup.find('div', {'class': 'rc-Language'})
         print(language.text)
+        #course_info.append({'language':language.text})
         rate = soup.find('div', {'class': 'ratings-text bt3-visible-xs'})
         if rate:
+            #course_info.append({'rate':rate.text})
             print(rate.text)
-        print(len(soup.findAll('div', {'class': 'week'})))
-
+        #course_info.append({'course_duration':len(soup.findAll('div', {'class': 'week'}))})
+        #course_list.append(course_info)
+    #print(len(course_list))
+"""
 
 def output_courses_info_to_xlsx(filepath):
     pass
