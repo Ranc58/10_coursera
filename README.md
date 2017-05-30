ca57a81ef91bf3f850a18344cbaf3640b243d26a
Ranc58 <rancvova@gmail.com>
2017-05-30 13:27:23 +0300

    rebuild get_course_info for using beautifulsoup4 edit main for exceptions handling and add user-friendly messages

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+This programm get 20 random courses from [coursera.org](https://coursera.org) and create .xlsx file in program folder with some information about course: 
+1. Course name
+2. Course language
+3. Start course date
+4. Course duration(on weeks)
+5. Course rate
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quick launch:
+
+Launching on Linux: 
+```bash
+$ python coursera.py
+Please wait.
+Coursera.xlsx was created in folder <dirpath>
+```
+Launching on Windows is same
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,9 +1,9 @@
+import os
 import random
 import requests
 from bs4 import BeautifulSoup as bs
 from lxml import html
 from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
 
 
 def get_random_courses_list(url):
@@ -19,18 +19,23 @@ def get_courses_info_list(random_courses_list):
     courses_info_list = []
     for course in random_courses_list:
         doc = requests.get(course)
-        course_content = html.fromstring(doc.content)
-        course_name = '//h1[@class="title display-3-text"]/text()'
-        start_date = '//div[@class="startdate rc-StartDateString caption-text"]/span/text()'
-        course_lang = '//div[@class="rc-Language"]/text()'
-        course_rate = '//div[@class="ratings-text bt3-visible-xs"]/text()'
-        course_duration = 'count(//*[@class="week"])'
+        doc.encoding = 'UTF-8'
+        parse_info = bs(''.join(doc.text), "lxml")
+        course_name = parse_info.find('h1', {'class': 'title display-3-text'})
+        start_date = parse_info.find(
+            'div', {'class': 'startdate rc-StartDateString caption-text'}
+            )
+        language = parse_info.find('div', {'class': 'rc-Language'})
+        rate = parse_info.find('div', {'class': 'ratings-text bt3-visible-xs'})
+        course_duration = len(parse_info.findAll('div', {'class': 'week'}))
         course_info = {
-            'course_name': ''.join(course_content.xpath(course_name)),
-            'starting_date': ''.join(course_content.xpath(start_date)),
-            'course_lang': ''.join(course_content.xpath(course_lang)),
-            'course_rate': ''.join(course_content.xpath(course_rate)),
-            'course_duration': course_content.xpath(course_duration)
+            'course_name': course_name.text,
+            'starting_date': start_date.text,
+            'course_lang': language.text,
+            'course_rate': rate.text
+            if rate is not None
+            else 'Not rated yet',
+            'course_duration': course_duration
         }
         courses_info_list.append(course_info)
     return courses_info_list
@@ -42,7 +47,7 @@ def output_courses_info_to_xlsx(courses_info_list):
     ws['A1'] = 'Course name'
     ws['B1'] = 'Course language'
     ws['C1'] = 'Start course date'
-    ws['D1'] = 'Course duration(weeks)'
+    ws['D1'] = 'Course duration(on weeks)'
     ws['E1'] = 'Course rate'
     for course in courses_info_list:
         ws.append([course['course_name'],
@@ -50,14 +55,20 @@ def output_courses_info_to_xlsx(courses_info_list):
                    course['starting_date'],
                    course['course_duration'],
                    course['course_rate']
-                   if course['course_rate'] != ''
-                   else 'Not rated yet',
                    ])
     wb.save('Coursera.xlsx')
 
 
 if __name__ == '__main__':
     url = 'http://www.coursera.org/sitemap~www~courses.xml'
-    random_courses_list = get_random_courses_list(url)
-    courses_info_list = get_courses_info_list(random_courses_list)
-    output_courses_info_to_xlsx(courses_info_list)
+    try:
+        print('Please wait.')
+        random_courses_list = get_random_courses_list(url)
+        courses_info_list = get_courses_info_list(random_courses_list)
+    except ValueError:
+        print('Error! Please check xml URL')
+    except requests.exceptions.ConnectionError:
+        print('Error!Please check your connection')
+    else:
+        output_courses_info_to_xlsx(courses_info_list)
+        print('Coursera.xlsx was created in folder', os.getcwd())
